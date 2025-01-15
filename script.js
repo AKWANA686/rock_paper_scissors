6bbc45d8e8425e656b9bb7c33390a22df0bba665
Akwana686 <akwanaoliver@gmail.com>
2025-01-15 11:07:53 +0300

    ROCK_PAPER_SCISSORS PROJECT

---
+++ script.js
@@ -0,0 +1,23 @@
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 0) {
+        return "Rock";
+    } else if (randomNumber === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+function getHumanChoice() {
+    let choice = prompt("Rock", "Paper", "Scissors");
+    const randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        return "Rock";
+    } else if (randomNumber === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
